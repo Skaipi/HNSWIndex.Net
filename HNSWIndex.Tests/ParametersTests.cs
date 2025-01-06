b453f870cd2baf58a0f3041ccf3a0fe861715e25
Mateusz Skarupski <skaiipii@gmail.com>
2025-01-06 20:38:50 +0100

    Add parameters tests

--- HNSWIndex.Tests/ParametersTests.cs
+++ HNSWIndex.Tests/ParametersTests.cs
@@ -40,6 +40,88 @@ namespace HNSWIndex.Tests
             Assert.IsTrue(recall > 0.90);
         }
 
+
+        [TestMethod]
+        public void TestParameterMinNN()
+        {
+            Assert.IsNotNull(vectors);
+
+            var parameters = new HNSWParameters<float> { MinNN = 1 };
+            var index = new HNSWIndex<float[], float>(Metrics.CosineMetric.Compute, parameters);
+
+            for (int i = 0; i < vectors.Count; i++)
+            {
+                Utils.Normalize(vectors[i]);
+                index.Add(vectors[i]);
+            }
+
+            var goodFinds = 0;
+            for (int i = 0; i < vectors.Count; i++)
+            {
+                var result = index.KnnQuery(vectors[i], 1);
+                var bestItem = result[0].Label;
+                if (vectors[i] == bestItem)
+                    goodFinds++;
+            }
+
+            var recall = (float)goodFinds / vectors.Count;
+            Assert.IsTrue(recall > 0.70 && recall < 0.90);
+        }
+
+        [TestMethod]
+        public void TestParameterMaxCandidates()
+        {
+            Assert.IsNotNull(vectors);
+
+            var parameters = new HNSWParameters<float> { MaxCandidates = 32 };
+            var index = new HNSWIndex<float[], float>(Metrics.CosineMetric.Compute, parameters);
+
+            for (int i = 0; i < vectors.Count; i++)
+            {
+                Utils.Normalize(vectors[i]);
+                index.Add(vectors[i]);
+            }
+
+            var goodFinds = 0;
+            for (int i = 0; i < vectors.Count; i++)
+            {
+                var result = index.KnnQuery(vectors[i], 1);
+                var bestItem = result[0].Label;
+                if (vectors[i] == bestItem)
+                    goodFinds++;
+            }
+
+            var recall = (float)goodFinds / vectors.Count;
+            Assert.IsTrue(recall > 0.90);
+        }
+
+        [TestMethod]
+        public void TestParameterLowRecall()
+        {
+            Assert.IsNotNull(vectors);
+
+            var parameters = new HNSWParameters<float> { MaxEdges = 8, MinNN = 1, MaxCandidates = 16 };
+            var index = new HNSWIndex<float[], float>(Metrics.CosineMetric.Compute, parameters);
+
+            for (int i = 0; i < vectors.Count; i++)
+            {
+                Utils.Normalize(vectors[i]);
+                index.Add(vectors[i]);
+            }
+
+            var goodFinds = 0;
+            for (int i = 0; i < vectors.Count; i++)
+            {
+                var result = index.KnnQuery(vectors[i], 1);
+                var bestItem = result[0].Label;
+                if (vectors[i] == bestItem)
+                    goodFinds++;
+            }
+
+            var recall = (float)goodFinds / vectors.Count;
+            Assert.IsTrue(recall < 0.50);
+        }
+
         public static List<int> BruteForceHeuristic(List<NodeDistance<float>> candidates, Func<int, int, float> distanceFnc, int maxEdges)
         {
             return candidates.OrderBy(x => -x.Dist).Take(maxEdges).ToList().ConvertAll(x => x.Id);
