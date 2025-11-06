80e00ac93706b5a94b60674b17a6ae1a7edb4636
Mateusz Skarupski <skaiipii@gmail.com>
2025-11-06 05:48:41 +0100

    Remove unused imports

--- src/HNSWIndex/GraphNavigator.cs
+++ src/HNSWIndex/GraphNavigator.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Numerics;
+﻿using System.Numerics;
 
 namespace HNSWIndex
 {
--- src/HNSWIndex/NestedListWrapper.cs
+++ src/HNSWIndex/NestedListWrapper.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using ProtoBuf;
 
 namespace HNSWIndex
